fadd94bf39eb2df5cba6e0b7915d2b6543453fe4
lrnflx <lorenefelx@gmail.com>
2019-03-05 13:40:54 +0100

    frontend

--- agenceImmo/src/Controller/HomeController.php
+++ agenceImmo/src/Controller/HomeController.php
@@ -2,26 +2,25 @@
 
 namespace App\Controller;
 
-use Twig\Environment;
+
+use App\Repository\PropertyRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController; 
 
 class HomeController extends AbstractController
 {   
-    /**
-     * @var Environment
-     */
-    public function __construct(Environment $twig)
-    {
-        $this->twig = $twig;
-    }
+
     /**
      * @Route("/", name="home")
+     * @param PropertyRepository $repository
      * return Response
      */
-    public function index()
+    public function index(PropertyRepository $repository): Response
     {
-        return $this->render('pages/home.html.twig');
+        $properties = $repository->findLatest();
+        return $this->render('pages/home.html.twig', [
+            'properties' => $properties
+        ]);
     }
 }
--- agenceImmo/src/Controller/PropertyController.php
+++ agenceImmo/src/Controller/PropertyController.php
@@ -2,21 +2,56 @@
 
 namespace App\Controller;
 
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PropertyController extends AbstractController
 {   
+    /**
+     * @var PropertyRepository
+     */
+    private $repository;
+
+    public function __construct(PropertyRepository $repository, ObjectManager $em)
+    {
+        $this->repository = $repository;
+        $this->em = $em;
+    }
 
     /**
      * @Route("/biens", name="properties_show")
      * @return Response
      */
     public function index(): Response
-    {
+    {   
+        $property = $this->repository->findAllVisible();
+        dump($property);
         return $this->render('property/index.html.twig' , [
             'current_menu' => 'properties' 
+        ]);
+    }
+
+    /**
+     * @Route("/biens/{slug}-{id}", name="property_show", requirements={"slug" = "[a-z0-9\-]*"})
+     */
+    public function show(Property $property, string $slug): Response
+    {   
+        if($property->getSlug() !== $slug)
+        {
+           return $this->redirectToRoute('property_show', [
+                'id' => $property->getId(),
+                'slug' => $property->getSlug()
+            ], 301 );
+        }
+
+        // $property =  $this->repository->find($id);
+        return $this->render('property/show.html.twig', [
+            'current_menu' => 'properties', 
+            'property' => $property
         ]);
     }
 
